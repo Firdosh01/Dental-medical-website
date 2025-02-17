96889710c8b0a510ad9371d79be933151ac41437
Firdosh01 <mdfirdosha37@gmail.com>
2025-02-17 21:35:57 +0530

    routes added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,23 @@
+import { Route, Routes } from "react-router-dom";
+/* pages */
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Services from "./pages/Services";
+import Contact from "./pages/Contact";
+import Navbar from "./components/common/Navbar";
+
 const App = () => {
-  return <div>Hello</div>;
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about-us" element={<About />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </>
+  );
 };
 
 export default App;
