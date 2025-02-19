1daa60441c3709afc59db36223a0a26a17cfefc9
Firdosh01 <mdfirdosha37@gmail.com>
2025-02-19 23:07:57 +0530

    changes

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -1,7 +1,73 @@
-import React from "react";
+import { NavLink } from "react-router-dom";
+import "./Navbar.scss";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="max_width_container">
+      <div className="navbar_container">
+        <div className="navbar">
+          <div>
+            <img src="/assets/logos/logo.png" alt="" />
+          </div>
+          <nav>
+            <ul className="flex gap-[38px] navigation_links">
+              <li>
+                <NavLink
+                  to="/"
+                  className={({ isActive }) =>
+                    `b2_medium text-[var(--dark-blue-color)] ${
+                      isActive ? "b2_semibold !b2_medium" : ""
+                    }`
+                  }
+                >
+                  Home
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to="/about"
+                  className={({ isActive }) =>
+                    `b2_medium text-[var(--dark-blue-color)] ${
+                      isActive ? "b2_semibold !b2_medium" : ""
+                    }`
+                  }
+                >
+                  About
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to="/services"
+                  className={({ isActive }) =>
+                    `b2_medium text-[var(--dark-blue-color)] ${
+                      isActive ? "b2_semibold !b2_medium" : ""
+                    }`
+                  }
+                >
+                  Services
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to="/contact"
+                  className={({ isActive }) =>
+                    `b2_medium text-[var(--dark-blue-color)] ${
+                      isActive ? "b2_semibold !b2_medium" : ""
+                    }`
+                  }
+                >
+                  Contact
+                </NavLink>
+              </li>
+            </ul>
+          </nav>
+          <div>
+            <button>Book Now</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
